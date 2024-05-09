66cecbb43ce5f9ab2aef07c474d83bbc6c20871d
Rafael Minaya <rafael_mr20@outlook.com>
2024-05-09 00:31:44 -0500

    Configuracion del proyecto con spring boot 2.1.17 y la dependencia de project reactor

---
+++ src/test/java/com/rminaya/springboot/reactor/app/SpringBootReactorApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rminaya.springboot.reactor.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+	
+	void contextLoads() {
+	}
+
+}
